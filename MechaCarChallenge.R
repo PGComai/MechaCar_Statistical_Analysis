b3446532a81299fd60b1c333bc75818dc1931c60
PGComai <comai.alex@gmail.com>
2023-01-21 14:45:48 -0800

    Update MechaCarChallenge.R

--- MechaCarChallenge.R
+++ MechaCarChallenge.R
@@ -1,3 +1,7 @@
 library(dplyr)
 
 mecha_mpg <- read.csv('MechaCar_mpg.csv')
+
+l_reg <- lm(mpg~AWD+ground_clearance+spoiler_angle+vehicle_weight+vehicle_length, data=mecha_mpg)
+
+reg_sum <- summary(l_reg)
